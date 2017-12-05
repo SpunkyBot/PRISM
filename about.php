dbc5211897421ced8f21d789056bdec5ee7d70a2
Alexander Kress <hello@alexanderkress.de>
2017-12-05 12:37:42 +0100

    Add template rules and about PHP files

---
+++ about.php
@@ -0,0 +1,18 @@
+<?php
+$title = 'About';
+
+// HTML Header
+require('./tpl/header.php');
+?>
+
+        <div class="page-header">
+          <h1>About us</h1>
+        </div>
+        <div>
+          <p>Your text here...</p>
+        </div>
+
+<?php
+// HTML Footer
+require('./tpl/footer.php');
+?>
---
+++ rules.php
@@ -0,0 +1,19 @@
+<?php
+$title = 'Rules';
+
+// HTML Header
+require('./tpl/header.php');
+?>
+
+        <div class="page-header">
+          <h1>Server Rules</h1>
+        </div>
+        <div>
+          <h3>Your headline</h3>
+          <p>Put your text here...</p>
+        </div>
+
+<?php
+// HTML Footer
+require('./tpl/footer.php');
+?>
