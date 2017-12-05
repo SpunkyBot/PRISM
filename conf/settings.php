9919d417cdcd3d02a5ed4bf7c55f280eaa0a66fd
Alexander Kress <hello@alexanderkress.de>
2017-12-05 14:43:21 +0100

    Add config file settings.php

---
+++ conf/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+// Define IPv4 address and port of the game server
+$ip = "127.0.0.1";
+$port = 27960;
+
+// Define the full path of the SQLite database
+$dbPath = "/opt/spunkybot/data.sqlite";
+
+?>
