bfadd1fce2ae4999891171258760b5b9005e6114
Alexander Kress <hello@alexanderkress.de>
2018-05-21 11:33:03 +0200

    Use escaping function to show version string

--- tpl/footer.php
+++ tpl/footer.php
@@ -1,7 +1,7 @@
 
     </div>
     <footer class="footer container text-center">
-      <div class="small">Copyright © 2018 PRISM <?php echo $version; ?> for <a href="https://www.spunkybot.de">Spunky Bot</a></div>
+      <div class="small">Copyright © 2018 PRISM <?php echo htmlspecialchars($version, ENT_QUOTES, 'UTF-8'); ?> for <a href="https://www.spunkybot.de">Spunky Bot</a></div>
     </footer>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha256-U5ZEeKfGNOja007MMD3YBI0A3OSZOQbeG6z2f2Y0hu8=" crossorigin="anonymous"></script>
